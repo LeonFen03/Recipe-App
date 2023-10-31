9d0619d9b50fb397dbbcc5260138d098c87a4e8b
LeonFen03 <LeonFen03@gmail.com>
2023-10-31 19:06:48 -0400

    Created Component folder containing Header.js/Header.css"

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+function Header () {
+    return (<div className="herosection-image-container">
+        <div className="title">
+           
+        </div>
+</div>)
+}
+export default Header;
--- src/components/Home/Home.js
+++
@@ -1,8 +0,0 @@
-import  './Home.css';
-
-function Home () {
-    return (<div className="herosection-image">
-        
-    </div>)
-}
-export default Home;
