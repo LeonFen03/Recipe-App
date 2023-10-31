6780050d07c5dd399d3d47415ab4b868a35e5ba7
LeonFen03 <LeonFen03@gmail.com>
2023-10-31 19:13:19 -0400

    Created Home component, added images for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
 import './Header.css';
+import title from '../images/dragonbites.png'
+import dragonIcon from '../images/dragonicon.png';
 function Header () {
     return (<div className="herosection-image-container">
         <div className="title">
-           
+            <div className="title-bg">
+                <img src={title} />
+                <img className="dragon" src={dragonIcon}  />
+            </div>
         </div>
 </div>)
 }
