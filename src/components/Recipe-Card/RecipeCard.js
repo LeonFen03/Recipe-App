75adaa3389420af19e9adc6361715b56b61683d0
LeonFen03 <LeonFen03@gmail.com>
2023-10-31 20:26:06 -0400

    Recipe-Card Add Recipe, Delete Recipe

--- src/components/Recipe-Card/RecipeCard.js
+++ src/components/Recipe-Card/RecipeCard.js
@@ -1,10 +1,14 @@
 import './RecipeCard.css';
 import {Card} from '@mui/material';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
-import CustomizedDialogs from '../Recipe-Card/RecipeDialogAction/RecipeCard';
-import { serverURL } from '../Root/Root';
-import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { Button} from '@mui/material';
+import { serverURL } from '../Root/Root';
+import { deleteFavorites } from '../RecipeMethods/RecipeMethods';
+import { Link } from 'react-router-dom';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 function RecipeCard (props) {
     const server =  !props.mockUp ? serverURL : '';
     const {favoritedStatus} = props;
@@ -12,18 +16,37 @@ function RecipeCard (props) {
     const [favoriteToggle,setFavoriteToggle] = useState(isFavorited);
     const image = props.image ? server+props.image : "https://placehold.co/310x400/000000/FFF";
 
-    console.log(image)
+    useEffect(() => {
+        if (isFavorited) {
+            setFavoriteToggle(favoritedStatus);
+        }
+    },[favoritedStatus]);
+
+    function handleToggle (recipes) {
+        if (favoriteToggle) {
+            const id = recipes["_id"] ? recipes["_id"] : recipes["id"];
+            deleteFavorites(id,props.setFavorite);
+        } else {
+            props.addFavorite(recipes);
+        }
+        setFavoriteToggle((prev) => !prev);
+    }
+
     return ( <motion.div
         key={props.key}
-        initial={{ opacity: 0, transform:`scale(0.9)` }}
+        initial={{ opacity: 0, transform:`scale(0.7)` }}
         animate={{ opacity: 1, transform:`scale(1)`}}
         exit={{ opacity: 0 }}
-        transition={{ duration: 1.7,type: "spring" }}
+        transition={{ duration: 1.9,type: "spring",bounce:0.5 }}
+
         ><Card elevation={15} className="recipe-card">
         <div className="recipe-title">
-           <h3 style={{fontSize:'1.4em'}}>{props.name}</h3>
-           <CustomizedDialogs image={image} recipeObject={props.recipe} text={<h3>Recipe <FastfoodIcon size='large' /></h3>} />
+           <h3 style={{fontSize:'1.4em'}}>{props.recipe.name}</h3>
+           <Link style={{color:'#471824'}} to={'/recipes/'+props.recipe["_id"]}><h3>Recipe <FastfoodIcon size='large' /></h3></Link>
 
+           <div className="favorited-bar">
+           {favoriteToggle ? <Button onClick={(e) => handleToggle(props.recipe)}> <FavoriteIcon sx={{color:'red',fontSize:'3em'}} /></Button> : <Button  onClick={(e) => handleToggle(props.recipe)}><FavoriteBorderIcon sx={{color:'red',fontSize:'3em'}} /></Button>}
+            </div>
         </div>
         <div className="recipe-image">
             <img src={image} />
