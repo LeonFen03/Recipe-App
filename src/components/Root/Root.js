4c37e2e997a8e0d6e1756794bbd7b63c9c4dc260
LeonFen03 <LeonFen03@gmail.com>
2023-10-31 21:21:19 -0400

    Routes for webpages

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,21 +1,28 @@
 import { Outlet } from "react-router";
+import { Button } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import './Root.css';
 
-
-
 function Root () {
- return (<div>
-    <nav className="navigation">
-        <ul>
-            <li>
-
-            </li>
-        </ul>
-    </nav>
-    <Outlet />
- </div>)
-}
-
+    const navigate = useNavigate()
+    // useEffect(() => {
+    //   navigate('/Home')
+    // },[])
+   return (<div>
+       <nav className="navigation">
+          <ul>
+              <li>
+                 <Button style={{borderRadius:'10px',backgroundColor:'rgba(138,89,78,255)'}} variant="contained" onClick={(e) => navigate('/Home')}>Home</Button>
+              </li>
+              <li>
+                <Button style={{borderRadius:'10px',backgroundColor:'rgba(138,89,78,255)'}} variant="contained" onClick={(e) => navigate('/Favorites')}>Login</Button>
+              </li>
+          </ul>
+          </nav>
+      <Outlet />
+   </div>)
+  }
 
 export const serverURL = 'http://localhost:3002';
 
