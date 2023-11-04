a7498325de81faed0dbbee1cdd0611f0028ae4ca
TGlock <100971383+Aruin1@users.noreply.github.com>
2023-11-04 16:22:39 -0400

    coments added on add recipe

--- src/components/AddRecipes/AddRecipe.js
+++ src/components/AddRecipes/AddRecipe.js
@@ -1,8 +1,12 @@
+//requirements
 import { useState } from "react";
 import { useEffect } from "react";
 import AddRecipeDialog from './AddRecipeDialog';
 import { addRecipe } from "../RecipeMethods/RecipeMethods";
+
+//function to add a recipe to the database 
 function AddRecipe ({ setRecipeList }) {
+    //establishing state variables for new recipe
     const [image,setImage] = useState('')
     const [name,setName] = useState('')
     const [instructions,setInstructions] = useState('')
@@ -14,6 +18,7 @@ function AddRecipe ({ setRecipeList }) {
         console.log(category)
     },[category])
 
+//function takes user input adding recipe to database
     function handleSubmit () {
         addRecipe({
             image:image,
