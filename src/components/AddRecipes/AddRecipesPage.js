6d5beebb1b76a5f43fd8f6f2eff09ef35067ce1c
TGlock <100971383+Aruin1@users.noreply.github.com>
2023-11-05 12:12:32 -0500

    added navigate functionality

--- src/components/AddRecipes/AddRecipesPage.js
+++ src/components/AddRecipes/AddRecipesPage.js
@@ -1,16 +1,28 @@
 import { motion } from "framer-motion";
 import './AddRecipe.css';
+import { useNavigate } from "react-router";
+import {Button} from "@mui/material";
 function AddRecipesPage () {
+    const navigate = useNavigate();
+
+    function goBack () {
+        navigate(-1);
+    }
+
+    
     return (<motion.div
     initial={{ opacity: 0, transform:`scale(0.7)` }}
     animate={{ opacity: 1, transform:`scale(1)`}}
     exit={{ opacity: 0 }}
     transition={{ duration: 1.9,type: "spring",bounce:0.5 }}
 
-    ><div>
+    ><div className="window-container">
+    <div className="recipes-container">
+        <div className="window-exit" style={{display:'flex',justifyContent:'space-between'}}>
+
 
         
-    </div></motion.div>)
+    </div></div></div></motion.div>)
 
 }
 
